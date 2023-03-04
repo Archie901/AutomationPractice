c30bbfefc22eefa5f8b9887296a2a7b2acd65dc9
Archie901 <107424612+Archie901@users.noreply.github.com>
2023-03-04 03:12:02 +0200

    nevermind

--- GundechaProject/PickMeTest.py
+++ GundechaProject/PickMeTest.py
@@ -9,35 +9,35 @@ class HomePageTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Firefox()
-        cls.driver.implicitly_wait(30)
+        cls.driver.implicitly_wait(2)
         cls.driver.get("https://www.saucedemo.com")
 
-    def test_login_toPage(self):
+    def test1_login_toPage(self):
         logo = self.driver.find_element(By.CLASS_NAME, "login_logo")
         self.assertTrue(logo)
         print(logo.text)
         inputLogin = self.driver.find_element(By.ID, "user-name").send_keys("standard_user")
         inputPass = self.driver.find_element(By.ID, "password").send_keys("secret_sauce")
-        btnLogin = self.driver.find_element(By.ID, "login-button").click()
+        btnLogin = self.driver.find_element(By.ID, "login-button").click();
     
-    def test_sortingThings(self):
+    def test2_sortingThings(self):
         self.assertTrue(self.driver.find_element(By.XPATH, "//span[text()='Products']"))
         sortingDropdown = Select(self.driver.find_element(By.CLASS_NAME, "product_sort_container"))
         selected_option = sortingDropdown.first_selected_option
         print(selected_option.text)
-        
-    def test_clicking_burgers(self):
-        
-
     
-    def test_whetherCartEmpty(self):
+    def test3_clickingBurger(self):
+        self.driver.find_element(By.ID, "react-burger-menu-btn").click()
+        self.driver.find_element(By.ID, "react-burger-cross-btn").click()
+    
+    def test4_whetherCartEmpty(self):
         self.driver.get("https://www.saucedemo.com/cart.html")
         try:
             self.driver.find_element(By.XPATH, "//div[@class='cart_list']//div[@class='cart_item']")
             print("Element is present")
         except NoSuchElementException:
             print("Element is NOT present")
-
+    
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
---
+++ GundechaProject/SuiteSample.py
@@ -0,0 +1,18 @@
+import unittest
+import HtmlTestRunner
+import os
+from NewFile2_test import SearchTest
+from PickMeTest import HomePageTest
+
+
+search_tests = unittest.TestLoader().loadTestsFromTestCase(SearchTest)
+
+home_page_tests = unittest.TestLoader().loadTestsFromTestCase(HomePageTest)
+
+smoke_tests = unittest.TestSuite([search_tests, home_page_tests])
+
+runner = HtmlTestRunner.HTMLTestRunner(verbosity=2, output='reports', title='Test report', report_name='report',
+                        open_in_browser=True, description="HTMLTestReport", tested_by="Artem Marchenko",
+                        add_traceback=False)
+
+runner.run(smoke_tests)
