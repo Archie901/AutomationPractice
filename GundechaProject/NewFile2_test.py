80e27554e4d35b758a727fbc221e9c60241beda4
Archie901 <107424612+Archie901@users.noreply.github.com>
2023-03-09 15:24:53 +0200

    2131321

--- GundechaProject/NewFile2_test.py
+++ GundechaProject/NewFile2_test.py
@@ -5,14 +5,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 class SearchTest(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Firefox()
         cls.driver.implicitly_wait(30)
         cls.driver.get("https://www.saucedemo.com")
         cls.driver.title
-
     def test_searchingItems(self):
         inputLogin = self.driver.find_element(By.ID, "user-name").send_keys("standard_user")
         inputPass = self.driver.find_element(By.ID, "password").send_keys("secret_sauce")
@@ -22,11 +20,9 @@ class SearchTest(unittest.TestCase):
         for product in products:
             print(product.text)
         self.assertEqual(6, len(products), '332433222222222222222222')
-    
     def test_something_new(self):
         sorting1 = self.driver.find_element(By.XPATH, "//select//option[@value='lohi']").click()
         sorting2 = self.driver.find_element(By.XPATH, "//select//option[@value='za']").click()
-
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
