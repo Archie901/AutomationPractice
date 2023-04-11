209c7a32c9b873f0475566379e2d955ba7d7045b
Archie901 <107424612+Archie901@users.noreply.github.com>
2023-04-11 17:57:49 +0300

    dfgdfa
    dfgdfadfgdfadfgdfadfgdfa

--- 123_test.py
+++ 123_test.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+'''driver = webdriver.Chrome()
 
 driver.get("https://www.google.com/")
 
@@ -15,4 +15,4 @@ input_google.send_keys(Keys.RETURN)
 
 assert "Iphone - Поиск в Google" in driver.title
 
-time.sleep(2)
+time.sleep(2)'''
