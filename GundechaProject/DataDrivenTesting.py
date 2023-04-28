c23917dc507b218052219520aa349f040328edf3
Archie901 <107424612+Archie901@users.noreply.github.com>
2023-04-28 20:35:42 +0300

    deleted log ifllf

--- GundechaProject/DataDrivenTesting.py
+++ GundechaProject/DataDrivenTesting.py
@@ -5,22 +5,36 @@ from ddt import ddt, data, unpack
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+<<<<<<< HEAD
+'''def get_dataCSV(filename):
+=======
 def get_dataCSV(filename):
+>>>>>>> innovation_first
     rows = []
     datafile = open(filename, "r")
     reader = csv.reader(datafile)
     next(reader, None)
     for row in reader:
         rows.append(row)
+<<<<<<< HEAD
+    return rows'''
+
+def get_dataEXCEL(filename):
+=======
     return rows
 
 '''def get_dataEXCEL(filename):
+>>>>>>> innovation_first
     rows = []
     book = xlrd.open_workbook(filename)
     sheet = book.sheet_by_index(0)
     for row_idx in range(1, sheet.nrows):
         rows.append(list(sheet.row_values(row_idx, 0, sheet.ncols)))
+<<<<<<< HEAD
+    return rows
+=======
     return rows'''
+>>>>>>> innovation_first
 
 @ddt
 class UsingDDT(unittest.TestCase):
@@ -55,7 +69,11 @@ class UsingDDT(unittest.TestCase):
         #self.assertAlmostEqual
         self.assertGreaterEqual(expected_count, len(products), "not greater or equal")'''
     
+<<<<<<< HEAD
+    '''@data(*get_data("GundechaProject\DataDDT.csv"))
+=======
     @data(*get_dataCSV("GundechaProject\DataDDT.csv"))
+>>>>>>> innovation_first
     @unpack
     def test3_searchWith_CSV_File(self, search_value, expected_count):
         search_box = self.driver.find_element(By.XPATH, "//input[@type='search' and @class='Dm7py']")
@@ -70,9 +88,15 @@ class UsingDDT(unittest.TestCase):
             self.assertGreaterEqual(len(products), expected_count, "not greater or equal")
         else:
             msg = self.driver.find_element(By.XPATH, "//span[contains(text(), 'назву товару по-іншому')]")
+<<<<<<< HEAD
+            self.assertEqual("Спробуйте написати назву товару по-іншому, скоротити запит або перейдіть в категорію", msg.text)'''
+
+    @data(*get_dataEXCEL("GundechaProject\DataDDT.xls"))
+=======
             self.assertEqual("Спробуйте написати назву товару по-іншому, скоротити запит або перейдіть в категорію", msg.text)
 
     '''@data(*get_dataEXCEL("GundechaProject\DataDDT.xls"))
+>>>>>>> innovation_first
     @unpack
     def test3_searchWith_XLS_File(self, search_value, expected_count):
         search_box = self.driver.find_element(By.XPATH, "//input[@type='search' and @class='Dm7py']")
@@ -87,7 +111,11 @@ class UsingDDT(unittest.TestCase):
             self.assertGreaterEqual(len(products), expected_count, "not greater or equal")
         else:
             msg = self.driver.find_element(By.XPATH, "//span[contains(text(), 'назву товару по-іншому')]")
+<<<<<<< HEAD
+            self.assertEqual("Спробуйте написати назву товару по-іншому, скоротити запит або перейдіть в категорію", msg.text)
+=======
             self.assertEqual("Спробуйте написати назву товару по-іншому, скоротити запит або перейдіть в категорію", msg.text)'''
+>>>>>>> innovation_first
 
     def tearDown(self):
         self.driver.quit()
