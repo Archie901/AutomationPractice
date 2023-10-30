6a1500a2d35e73c5ee3e4a24207e26d31d5c9006
Archie901 <107424612+Archie901@users.noreply.github.com>
2023-10-30 16:31:29 +0200

    added some scripts for tqrc

---
+++ TrueQRcode/scan_automation2.py
@@ -0,0 +1,21 @@
+import time
+from selenium import webdriver
+
+
+qr_id1 = 'FFWATS9Z'
+
+qr_id2 = 'KF2P98ZB'
+
+iterations = 30
+
+driver = webdriver.Edge()
+
+count = 0
+while True:
+    count += 1
+    driver.get("https://trueqrcode-dev.osdb.io/qr/" + qr_id2)
+    time.sleep(6)
+    if count == iterations:
+        break
+
+driver.quit()
