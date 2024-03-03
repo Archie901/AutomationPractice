5f6e509667fe3f452559880be9487801ce55872c
Archie901 <107424612+Archie901@users.noreply.github.com>
2024-03-03 14:05:10 +0200

    changes in main

--- TrueQRcode/automation_ui/_main.py
+++ TrueQRcode/automation_ui/_main.py
@@ -1,5 +1,9 @@
 import sys
 
-project_parent= 'D:\Zusefulings\DevelopTesting\GitHub\AutomationPractice\Trueqrcode'
+project_parent1 = 'D:\Zusefulings\DevelopTesting\GitHub\AutomationPractice\Trueqrcode'
 
-sys.path.append(project_parent)
+sys.path.append(project_parent1)
+
+project_parent2 = r'C:\Users\Archie-Lenovo\Documents\GitHub\AutomationPractice\Trueqrcode'
+
+sys.path.append(project_parent2)
