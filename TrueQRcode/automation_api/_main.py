5056379afdbc7a0213bc44ee416280df0ecbb3f9
Archie-Lenovo <arch@mailinator.com>
2024-06-29 15:19:50 +0300

    changed main module a bit

--- TrueQRcode/automation_api/_main.py
+++ TrueQRcode/automation_api/_main.py
@@ -1,9 +1,9 @@
 import sys
 
-project_parent1 = 'D:\Zusefulings\DevelopTesting\GitHub\AutomationPractice\Trueqrcode'
+project_parent1 = r'D:\Zusefulings\DevelopTesting\GitHub\AutomationPractice\Trueqrcode'
 
 sys.path.append(project_parent1)
 
-project_parent2 = r'C:\Users\Archie-Lenovo\Documents\GitHub\AutomationPractice\Trueqrcode'
+project_parent2 = r'D:\_testing\GitHub\AutomationPractice\TrueQRcode'
 
 sys.path.append(project_parent2)
