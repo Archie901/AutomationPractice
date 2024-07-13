b5c9971853ba86e49bd7a89aa77a810d8cb9b4a0
Archie901 <107424612+Archie901@users.noreply.github.com>
2024-07-14 02:51:31 +0300

    131

---
+++ NavierrePablo/tests/_main.py
@@ -0,0 +1,9 @@
+import sys
+
+project_parent1 = r'D:\Zusefulings\DevelopTesting\GitHub\AutomationPractice\NavierrePablo'
+
+sys.path.append(project_parent1)
+
+project_parent2 = r'D:\_testing\GitHub\AutomationPractice\NavierrePablo'
+
+sys.path.append(project_parent2)
