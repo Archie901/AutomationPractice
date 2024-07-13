aa991fee03717ac172fbb487df29e5c7faa07deb
Archie-Lenovo <arch@mailinator.com>
2024-07-14 02:38:35 +0300

    improved str for archie-lenovo

--- NavierrePablo/automation_ui/_main.py
+++ NavierrePablo/automation_ui/_main.py
@@ -4,6 +4,6 @@ project_parent1 = r'D:\Zusefulings\DevelopTesting\GitHub\AutomationPractice\Navi
 
 sys.path.append(project_parent1)
 
-project_parent2 = r'C:\Users\Archie-Lenovo\Documents\GitHub\AutomationPractice\NavierrePablo'
+project_parent2 = r'D:\_testing\GitHub\AutomationPractice\NavierrePablo'
 
 sys.path.append(project_parent2)
