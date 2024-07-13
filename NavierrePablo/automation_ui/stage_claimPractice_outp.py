6f5eb7a2859f5a79d1d21013d14204171cae480c
Archie901 <107424612+Archie901@users.noreply.github.com>
2024-07-14 00:48:37 +0300

    changed names

--- NavierrePablo/automation_ui/stage_claimPractice_outp.py
+++ NavierrePablo/automation_ui/stage_claimPractice_outp.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
-import elements_locators as el
+import locators as el
 import testvalues as tv
 
 service = Service()
